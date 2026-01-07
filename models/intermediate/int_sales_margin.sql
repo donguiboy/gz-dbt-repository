ade6ec34e7a3542a4772a1f6b9801d4dea45dd6a
Gonzalo Lara <gflara6@gmail.com>
2026-01-07 01:23:02 +0000

    dbt basics and dbt advance problems solved

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,12 @@
-SELECT 
-    *,
-    (sales.quantity * product.purchase_price) as purchase_cost,
-    round((revenue -(sales.quantity * product.purchase_price)),2) as margin
-FROM {{ ref("stg_raw__sales")}} sales
-INNER JOIN {{ref("stg_raw__product")}} product USING (products_id)
+  SELECT
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      USING (products_id)
